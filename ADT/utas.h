7363eef424e019d8d87836efbd3f348b54be8431
rafi-sama <rafimaliki28@gmail.com>
2023-10-26 20:30:58 +0700

    Create utas.h

---
+++ ADT/utas.h
@@ -0,0 +1,33 @@
+#ifndef utas_H
+#define utas_H
+
+#include "kicauan.h"
+
+typedef Kicauan KicauanUtama; 
+/* KicauanUtama memiliki sifat yang sama seperti kicauan biasa*/
+
+typedef struct {   
+  Sentence pesan[280];
+  Sentence author;
+  int jumlahDisukai;
+  Time dateTime;
+} KicauanSambungan;
+/* KicauanSambungan tidak memiliki id dan tidak bisa dibalas*/
+
+typedef struct {
+    int IDUtas;
+    KicauanUtama Utama;
+    KicauanSambungan Sambungan[100];
+    int NSambungan;
+} Utas;
+/* Setiap utas memiliki id berurut yang dimulai dari 1,
+   Setiap utas terdiri dari kicauan utama dan array of kicauan sambungan
+   NSambungan adalah banyaknya utas sambungan*/
+
+typedef struct {
+    Utas Buffer[100];
+    int Neff;
+} TabUtas;
+/* Tab untuk menyimpan utas, memiiki Neff*/'
+
+#endif
