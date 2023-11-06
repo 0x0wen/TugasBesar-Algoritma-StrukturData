423212a91ffabb4eb744bb8d824c6bf2a9758a46
heisenberg <owentobias21@gmail.com>
2023-11-06 08:33:40 +0700

    add global variables

---
+++ global.h
@@ -0,0 +1,9 @@
+#include "pengguna.h"
+#include "kicauan.h"
+#include "utas.h"
+#include "balasan.h"
+
+extern Pengguna penggunaSekarang;
+extern TabPengguna dataPengguna;
+extern TabKicauan dataKicauan;
+extern TabUtas dataUtas;
