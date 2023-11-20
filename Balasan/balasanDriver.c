e9b6ab57c4e3b8f6f3838370cc2b88a50139326c
heisenberg <owentobias21@gmail.com>
2023-11-20 23:54:47 +0700

    add driver

---
+++ Balasan/balasanDriver.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include "balasan.h"
+#include "kicauan.h"
+
+int main()
+{
+    Kicauan K1;
+    Balasan B1, B2, B3;
+    tree T1;
+    createBalasan(K1);
+    addBalasan(&K1, &B1);
+    addBalasan(&K1, &B2);
+    addBalasan(&K1, &B3);
+    removeNode(&K1);
+    searchBalasan(&K1, &B2);
+    displayAllBalasan(T1, 1);
+
+    return 0;
+}
