28e6e935d9fcef191598a06459d1a4b2d512eb28
heisenberg <owentobias21@gmail.com>
2023-11-21 23:56:10 +0700

    update gaming

--- PermintaanTeman/permintaanTeman.h
+++ PermintaanTeman/permintaanTeman.h
@@ -10,9 +10,9 @@
 /* Definisi elemen dan address */
 typedef struct
 {
-    int prio;  /* [1..100], prioritas dengan nilai 1..100 (1 adalah prioritas adalah tertinggi) */
-    Pengguna info; /* elemen karakter */
-} infotype;
+    int prio;      /* popularitas pengguna */
+    Pengguna info; /* pengguna */
+} AnggotaTeman;
 typedef int address; /* indeks tabel */
 /* Contoh deklarasi variabel bertype PrioQueueChar : */
 /* Versi I : tabel dinamik, Head dan Tail eksplisit, ukuran disimpan */
@@ -27,7 +27,7 @@ typedef struct
 /* Catatan implementasi: T[0] tidak pernah dipakai */
 
 /* ********* AKSES (Selektor) ********* */
-/* Jika e adalah infotype dan Q adalah PrioQueueChar, maka akses elemen : */
+/* Jika e adalah AnggotaTeman dan Q adalah PrioQueueChar, maka akses elemen : */
 #define Prio(e) (e).prio
 #define Info(e) (e).info
 #define Head(Q) (Q).HEAD
@@ -61,12 +61,12 @@ void DealokasiPermintaanTeman(PermintaanTeman *Q);
 /* F.S. Q menjadi tidak terdefinisi lagi, MaxEl(Q) diset 0 */
 
 /* *** Primitif Add/Delete *** */
-void EnqueuePermintaanTeman(PermintaanTeman *Q, infotype X);
+void EnqueuePermintaanTeman(PermintaanTeman *Q, AnggotaTeman X);
 /* Proses: Menambahkan X pada Q dengan aturan priority queue, terurut membesar berdasarkan prio */
 /* I.S. Q mungkin kosong, tabel penampung elemen Q TIDAK penuh */
 /* F.S. X disisipkan pada posisi yang tepat sesuai dengan prioritas,
         TAIL "maju" dengan mekanisme circular buffer; */
-void DequeuePermintaanTeman(PermintaanTeman *Q, infotype *X);
+void DequeuePermintaanTeman(PermintaanTeman *Q, AnggotaTeman *X);
 /* Proses: Menghapus X pada Q dengan aturan FIFO */
 /* I.S. Q tidak mungkin kosong */
 /* F.S. X = nilai elemen HEAD pd I.S., HEAD "maju" dengan mekanisme circular buffer;
