87056cd675a780aa6605bf8c006f7d319dbd720d
heisenberg <owentobias21@gmail.com>
2023-11-23 21:58:23 +0700

    fix bug

--- Balasan/commandBalasan.c
+++ Balasan/commandBalasan.c
@@ -3,15 +3,16 @@
 // Command Balasan
 void BALAS(int IDKicau, int IDBalasan)
 {
-    if (!IsKicauanInTab(IDKicau))
+    Kicauan K = *searchKicauan(dataKicauan, IDKicau);
+    if (LIST_BALASAN(*searchKicauan(dataKicauan, IDKicau)) == NULL)
     {
         printf("Wah, tidak terdapat kicauan yang ingin Anda balas!");
     }
-    else if (IDBalasan != 1 && !isBalasanExistInKicauan(IDKicau, IDBalasan))
+    else if (IDBalasan != 1 && searchBalasan(LIST_BALASAN(K), IDBalasan) == NULL)
     {
         printf("Wah, tidak terdapat balasan yang ingin Anda balas!");
     }
-    else if (!isFriend(IdPengguna(penggunaSekarang), IDKicau) && PrivasiPengguna(cariPengguna(IDKicau)))
+    else if (isFriend(matrixTeman, ID_PENGGUNA(penggunaSekarang), PENULIS_BALASAN(*searchBalasan(searchKicauan(dataKicauan, IDKicau), IDBalasan))) == false && PRIVASI_PENGGUNA(*searchPengguna(dataPengguna, IDKicau)))
     {
         printf("Wah, akun tersebut merupakan akun privat dan anda belum berteman dengan akun tersebut!");
     }
@@ -19,48 +20,63 @@ void BALAS(int IDKicau, int IDBalasan)
     {
         if (IDBalasan == -1)
         {
+            printf("Masukkan Balasan:\n");
+            AcquireSentence();
+            printf("Selamat! balasan telah diterbitkan!\n");
+            printf("Detil balasan:\n");
+            printf("| ID = %d", ID_KICAU(K));
+            printf("\n| ");
+            printSentence(NAMA_PENGGUNA(penggunaSekarang));
+            printf("\n| ");
+            TulisDATETIME(WAKTU_BALASAN(*searchBalasan(LIST_BALASAN(K), IDBalasan)));
+            printf("\n| ");
+            printf("/n");
         }
-        printf("Masukkan Balasan:\n");
-        AcquireSentence();
-        printf("Selamat! balasan telah diterbitkan!\n");
-        printf("Detil balasan:\n");
-        printf("| ID = %d", anjay);
-        printf("\n| %d", anjay);
-        printf("\n| ");
-        printTime();
-        printf("\n| ");
-        printSentence();
-        printf("/n");
+        else
+        {
+            printf("Masukkan Balasan:\n");
+            AcquireSentence();
+            printf("Selamat! balasan telah diterbitkan!\n");
+            printf("Detil balasan:\n");
+            printf("| ID = %d", ID_KICAU(K));
+            printf("\n| ");
+            printSentence(NAMA_PENGGUNA(penggunaSekarang));
+            printf("\n| ");
+            TulisDATETIME(WAKTU_BALASAN(*searchBalasan(LIST_BALASAN(K), IDBalasan)));
+            printf("\n| ");
+            printf("/n");
+        }
     }
 };
 
 void CETAK_BALASAN(int IDKicau)
 {
-    if (jumlahBalasan(IDKicau) == 0)
+    if (LIST_BALASAN(*searchKicauan(dataKicauan, IDKicau)) == 0)
     {
         printf("Belum terdapat balasan apapun pada kicauan tersebut. Yuk balas kicauan tersebut!");
     }
-    else if (!isFriend(IdPengguna(penggunaSekarang), IDKicau) && PrivasiPengguna(cariPengguna(IDKicau)))
-    {
-        printf("Wah, kicauan tersebut dibuat oleh pengguna dengan akun privat!");
-    }
-    else if (IsKicauanInTab(IDKicau))
+    else if (isFriend(matrixTeman, ID_PENGGUNA(penggunaSekarang), getIDPengguna(dataPengguna, AUTHOR_KICAU(*searchKicauan(dataKicauan, IDKicau)))) == false && PRIVASI_PENGGUNA(*searchPengguna(dataPengguna, IDKicau)))
+        {
+            printf("Wah, kicauan tersebut dibuat oleh pengguna dengan akun privat!");
+        }
+    else if (LIST_BALASAN(*searchKicauan(dataKicauan, IDKicau)) == NULL)
     {
         printf("Tidak terdapat kicauan dengan id tersebut!");
     }
     else
     {
-        // anjay
+        printTreeBalasan(LIST_BALASAN(*searchKicauan(dataKicauan, IDKicau)), 0);
     }
 }
 
 void HAPUS_BALASAN(int IDKicau, int IDBalasan)
 {
-    if (!isBalasanExistInKicauan(IDKicau, IDBalasan))
+    Balasan *B = searchBalasan(LIST_BALASAN(*searchKicauan(dataKicauan, IDKicau)), IDBalasan);
+    if (B == NULL)
     {
         printf("Balasan tidak ditemukan");
     }
-    else if (PENULIS_BALASAN(searchBalasan(IDBalasan)))
+    else if (IsSentenceEqual(AUTHOR_BALASAN(*B), NAMA_PENGGUNA(penggunaSekarang)) == false)
     {
         printf("Hei, ini balasan punya siapa? Jangan dihapus ya!");
     }
--- Balasan/commandBalasan.h
+++ Balasan/commandBalasan.h
@@ -1,7 +1,8 @@
 #ifndef COMMAND_BALASAN_H
 #define COMMAND_BALASAN_H
 
-#include "../Kicauan/tabKicauan.h"
+#include "balasan.h"
+#include "../global.h"
 void BALAS(int IDKicau, int IDBalasan);
 void CETAK_BALASAN(int IDKicau);
 void HAPUS_BALASAN(int IDKicau, int IDBalasan);
